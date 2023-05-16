08af4012e11bcbedfba527e07e69ab51a56b107f
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2023-05-15 22:59:32 -0600

    Update pruebas.cpp

--- pruebas.cpp
+++ pruebas.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
     //prueabs
     int x,y;
-    cout<<"Ingrese numeros 1"<<endl;
+    cout<<"Ingrese numero 1"<<endl;
     cin>>x;
-    cout<<"Ingrese numeros 1"<<endl;
+    cout<<"Ingrese numero 2"<<endl;
     cin>>y;
-    cout<<"El resultado es el siguiente "<<y+x<<endl;
+    cout<<"El resultado "<<y+x<<endl;
     return 0;
 }

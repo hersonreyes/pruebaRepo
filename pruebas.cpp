8e094fccddf7958a13a0b359137620bae3d75f83
hersonreyes <54414689+hersonreyes@users.noreply.github.com>
2023-05-16 09:30:23 -0600

    Update pruebas.cpp

--- pruebas.cpp
+++ pruebas.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     //pruebas
-    int x,y;
+    int y,x;
     cout<<"Ingrese numeros 1"<<endl;
     cin>>x>>y;
     cout<<"El resultado es el siguiente"<<y+x<<endl;

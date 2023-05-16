564629efa702b924cf7ff7727de9cca60ce01090
HersonReyes <00216519@uca.edu.sv>
2023-05-15 23:04:02 -0600

    Edit

--- pruebas.cpp
+++ pruebas.cpp
@@ -6,6 +6,6 @@ int main(){
     int x,y;
     cout<<"Ingrese numeros 1"<<endl;
     cin>>x>>y;
-    cout<<"El resultado es el siguiente "<<y+x<<endl;
+    cout<<"El resultado es el siguiente "<<x+y<<endl;
     return 0;
 }

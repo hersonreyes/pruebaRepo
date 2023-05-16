3cb7e9f4fa65785779045271d482d0badd9d6b18
HersonReyes <00216519@uca.edu.sv>
2023-05-15 22:05:58 -0600

    Add: Cpp de ejemplo

---
+++ pruebas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Ingrese numeros"<<endl;
+    cin>>x>>y;
+    cout<<"El resultado es "<<x+y<<endl;
+    return 0;
+}
